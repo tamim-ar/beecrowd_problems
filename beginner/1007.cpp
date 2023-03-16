7b63044b0467d7896626ee54d316412fd3e595fc
Tamim Ahasan <tamimahsan67@gmail.com>
2023-03-16 23:33:01 +0600

    1007

---
+++ beginner/1007.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A, B, C, D, SUM;
+
+    cin >> A >> B >> C >> D;
+
+    SUM = A * B - C * D;
+
+    cout << "DIFERENCA = " << SUM << endl;
+    return 0;
+}
